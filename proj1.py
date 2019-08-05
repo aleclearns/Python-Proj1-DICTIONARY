7e76b04e4df75a387c87b9cf3231ba1be520b735
alec <alecneilcollado@gmail.com>
2019-08-05 23:48:46 +0800

    Updated codes for bugs in Proper nouns

--- proj1.py
+++ proj1.py
@@ -2,14 +2,20 @@ import json
 from difflib import get_close_matches
 
 data = json.load(open("data.json"))
-print("*********************\nDICTIONARY\n********************* \n")
+print("*********************\nDICTIONARY\n********************* \nto end type stop() or exit() ")
 x = 0
 while x < 1:
-    user_input = str(input("to end type stop() or exit() \nSearch >>>"))
+    user_input = str(input("Search >>>"))
 
     if user_input.lower() == "stop()" or user_input.lower() == "exit()":
         x = x+1
         print("***END")
+    elif user_input.title() in data:
+        print("***Upon searching in the database, the word you typed returned: \n{}".format(data[user_input.title()]))
+        break
+    elif user_input.upper() in data:
+        print("***Upon searching in the database, the word you typed returned: \n{}".format(data[user_input.upper()]))
+        break
     elif user_input.lower() not in data:
         print("***Not in database, did you mean...{} ".format(get_close_matches(user_input.lower(),data.keys())[0:3]))
     else:
