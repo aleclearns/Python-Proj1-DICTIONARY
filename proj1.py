6d268754470ed377cb91fcb597713bd92ff6cdc1
alec <alecneilcollado@gmail.com>
2019-08-05 23:56:16 +0800

    Updated codes for proper noun bugs

--- proj1.py
+++ proj1.py
@@ -12,11 +12,11 @@ while x < 1:
         print("***END")
     elif user_input.title() in data:
         print("***Upon searching in the database, the word you typed returned: \n{}".format(data[user_input.title()]))
-        break
+        continue
     elif user_input.upper() in data:
         print("***Upon searching in the database, the word you typed returned: \n{}".format(data[user_input.upper()]))
-        break
+        continue
     elif user_input.lower() not in data:
-        print("***Not in database, did you mean...{} ".format(get_close_matches(user_input.lower(),data.keys())[0:3]))
+        print("***Not in database, did you mean...{} ".format(get_close_matches(user_input.lower(), data.keys())[0:3]))
     else:
         print("***Upon searching in the database, the word you typed returned: \n{}".format(data[user_input.lower()]))
